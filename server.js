340d56cf4037fa8e749d61499ce4a006add08aec
aalokgupta <aalokgupta02@gmail.com>
2017-11-21 07:26:32 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+              pageTitle: "HomePage",
+              WelcomeMessage: "Hey, welcome to my first node server",
+              currentYear: new Date().getFullYear()
+            });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/help', (req, res) => {
+
+});
+
+app.get('/bad', (req, res) => {
+  res.send({BadRequest: "No page found, unable to handle request"});
+})
+
+const port_no = 3000;
+app.listen(port_no, () =>{
+  console.log('server started listening at port no '+port_no);
+});
